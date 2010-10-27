e802662f1278e27fe8376f29fdcf70c1f0d6b312
Ben Hamill <git-commits@benhamill.com>
2010-10-26 19:14:52 -0500

    Fully Object-oriented strategy set-up. Look, Ma! An API!

--- chapel_trasure_map_strategy.rb
+++ chapel_trasure_map_strategy.rb
@@ -1,92 +1,60 @@
-require 'player'
+require 'strategy'
 
-@games_over_100 = 0
-@games_array = []
-
-def run
-  p = Player.new
-  @p = p
-
-  tm_count = 0
-  chapel_count = 0
-  played_treasure_maps = false
-  copper_count = 7
-  p_count = 0
-
-  while true do
-    # puts "Turn ##{p.turn_number}"
-    # puts "Hand: #{p.hand.inspect}"
-
-    if p.turn_number > 100
-      # puts "Exiting for too many turns."
-      @games_over_100 += 1
-      break
-    end
-
-    #action phase
-    if p.hand.count(:treasure_map) == 2
-      p.treasure_map
-      # puts "Playing Treasure Maps."
-      if played_treasure_maps
-        # puts "Playing second TMs."
-      else
-        played_treasure_maps = true
-      end
-    elsif p.hand.include? :chapel
-      keepers = [:chapel, :treasure_map, :province, :gold]
-
-      keepers << :copper if copper_count <= 4 and tm_count < 2
-
-      cards = p.hand.reject { |card| keepers.include? card }
-      cards.delete_at(cards.index(:copper)) if cards.count(:copper) >= 4 and tm_count < 2
-      copper_count -= cards.count(:copper)
-      p.chapel(*cards)
-      # puts "Playing Chapel with: #{cards.inspect}"
-    end
-
-    #buy phase
-    if tm_count < 2 and p.hand_value >= 4
-      p.buy :treasure_map
-      # puts "Buying Treasure Map."
-      tm_count += 1
-    elsif chapel_count < 1 and p.hand_value >= 2
-      p.buy :chapel
-      # puts "Buying Chapel."
-      chapel_count += 1
-    elsif p.hand_value >= 8
-      p.buy :province
-      # puts "Buying Province."
-      p_count += 1
-    elsif p.hand_value >= 6
-      p.buy :gold
-    end
-
-    if p_count >= 6
-      @games_array << p.turn_number
-      # puts p.inspect
-      break
-    end
-
-    p.end_turn
+class ChapelTreasureMap < Strategy
+  def counts
+    @tm_count = 0
+    @chapel_count = 0
+    @played_treasure_maps = false
+    @copper_count = 7
+    @p_count = 0
   end
 
-  # puts p.inspect
+  def action_phase
+    if @p.hand.count(:treasure_map) == 2
+      @p.treasure_map
+      puts "Playing Treasure Maps." if verbose?
+      if @played_treasure_maps
+        puts "Playing second TMs." if verbose?
+      else
+        @played_treasure_maps = true
+      end
+    elsif @p.hand.include? :chapel
+      keepers = [:chapel, :treasure_map, :province, :gold]
+
+      keepers << :copper if @copper_count <= 4 and @tm_count < 2
+
+      cards = @p.hand.reject { |card| keepers.include? card }
+      cards.delete_at(cards.index(:copper)) if cards.count(:copper) >= 4 and @tm_count < 2
+      @copper_count -= cards.count(:copper)
+      @p.chapel(*cards)
+      puts "Playing Chapel with: #{cards.inspect}" if verbose?
+    end
+  end
+
+  def buy_phase
+    if @tm_count < 2 and @p.hand_value >= 4
+      @p.buy :treasure_map
+      puts "Buying Treasure Map." if verbose?
+      @tm_count += 1
+    elsif @chapel_count < 1 and @p.hand_value >= 2
+      @p.buy :chapel
+      puts "Buying Chapel." if verbose?
+      @chapel_count += 1
+    elsif @p.hand_value >= 8
+      @p.buy :province
+      puts "Buying Province." if verbose?
+      @p_count += 1
+    elsif @p.hand_value >= 6
+      @p.buy :gold
+      puts "Buying Gold." if verbose?
+    end
+  end
+
+  def stop_conditions
+    if @p_count >= 6
+      @games_array << @p.turn_number
+      puts @p.inspect if verbose?
+      return true
+    end
+  end
 end
-
-# 100_000.times do
-#   run
-#   if @p.turn_number == 12
-#     puts @p.inspect
-#     break
-#   end
-# end
-
-100_000.times { run }
-
-# run
-
-puts @p.inspect
-
-puts "Games over 100 turns: #{@games_over_100}"
-puts "Games under 100 turns: #{@games_array.length}"
-puts "Min: #{@games_array.min} Max: #{@games_array.max} Avg: #{@games_array.inject(0.0) { |total, item| total += item} / @games_array.length }"
--- strategy.rb
+++ strategy.rb
@@ -1,24 +1,31 @@
 require 'player'
 
-module Strategy
-  def initialize
+class Strategy
+  def initialize verbose = false
     @games_over_100 = 0
     @games_array = []
+    @verbose = verbose
   end
 
-  def run iterations
+  def verbose?
+    !!@verbose
+  end
+
+  def run iterations=100_000
     iterations.times { play_game }
+    report(iterations)
   end
 
   def play_game
     @p = Player.new
+    counts
 
     loop do
-      # puts "Turn ##{@p.turn_number}"
-      # puts "Hand: #{@p.hand.inspect}"
+      puts "Turn ##{@p.turn_number}" if verbose?
+      puts "Hand: #{@p.hand.inspect}" if verbose?
 
       if @p.turn_number > 100
-        # puts "Exiting for too many turns."
+        puts "Exiting for too many turns." if verbose?
         @games_over_100 += 1
         break
       end
@@ -31,9 +38,10 @@ module Strategy
     end
   end
 
-  def report
+  def report iterations
     puts @p.inspect
 
+    puts "Games played: #{iterations}"
     puts "Games over 100 turns: #{@games_over_100}"
     puts "Games under 100 turns: #{@games_array.length}"
     puts "Min: #{@games_array.min} Max: #{@games_array.max} Avg: #{@games_array.inject(0.0) { |total, item| total += item} / @games_array.length }"
